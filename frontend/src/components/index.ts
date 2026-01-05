4d9e71e614b8c3de5719b9ce24f8fd15812649f9
Sharon Akande <akande.iyanusha.joy@gmail.com>
2026-01-05 16:18:05 +0100

    Improve responsive design for mobile and tablet - export from barrel files

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,3 @@
+export { Responsive_design } from './responsive_design';
+export { Responsive_designItem } from './responsive_design_item';
+export { Responsive_designList } from './responsive_design_list';
