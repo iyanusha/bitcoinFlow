7996ab5c9f487aefed6350df4f3e874768a4b31a
Sharon Akande <akande.iyanusha.joy@gmail.com>
2026-01-06 16:18:05 +0100

    Add SEO meta tags and structured data - export from barrel files

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,6 @@
 export { Responsive_design } from './responsive_design';
 export { Responsive_designItem } from './responsive_design_item';
 export { Responsive_designList } from './responsive_design_list';
+export { Seo_meta_tags } from './seo_meta_tags';
+export { Seo_meta_tagsItem } from './seo_meta_tags_item';
+export { Seo_meta_tagsList } from './seo_meta_tags_list';
