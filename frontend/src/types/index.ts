1a66a3a91f1b1fa37c94b7acafdfbdfda1a4cfb1
Sharon Akande <akande.iyanusha.joy@gmail.com>
2026-03-21 10:18:28 +0100

    Add about and team information - commit 19/50

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -293,3 +293,5 @@ export interface Faq_help_pageD38 { id: string; name: string; value: number; ts:
 export interface Faq_help_pageD48 { id: string; name: string; value: number; ts: number; }
 
 export interface About_pageD8 { id: string; name: string; value: number; ts: number; }
+
+export interface About_pageD18 { id: string; name: string; value: number; ts: number; }
