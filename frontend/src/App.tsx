4d29137e4b1b42ab70008a25420da5503f606fe8
Sharon Akande <akande.iyanusha.joy@gmail.com>
2026-03-22 12:19:30 +0100

    Dismiss error banner with Escape key via useKeyboard hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import { useTransactionHistory } from './hooks/useTransactionHistory'
 import { useToast } from './hooks/useToast'
 import { useTransactionStatus } from './hooks/useTransactionStatus'
 import { useDocumentTitle } from './hooks/useDocumentTitle'
+import { useKeyboard } from './hooks/useKeyboard'
 import { openContractCall } from '@stacks/connect'
 import { uintCV, PostConditionMode } from '@stacks/transactions'
 import { CONTRACT_ADDRESS, CONTRACT_NAME, network, getAddressExplorerUrl, getContractExplorerUrl } from './lib/stacks'
@@ -168,6 +169,7 @@ function App() {
 
   const pendingCount = transactions.filter(tx => tx.status === 'pending').length;
   useDocumentTitle(pendingCount > 0 ? `(${pendingCount}) BitcoinFlow` : 'BitcoinFlow');
+  useKeyboard('Escape', () => setError(null), !!error);
 
   return (
     <div className="app">
